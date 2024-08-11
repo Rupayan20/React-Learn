689b6b64dbb6868699a837c25db679978afd6bb4
Rupayan20 <duttarupayan08@gmail.com>
2024-08-11 11:08:46 +0530

    Added React Roadmap

---
+++ react-roadmap/roadmap.js
@@ -0,0 +1,58 @@
+/*
+React Roadmap
+
+=> Why to learn React?
+ - hype, job, trend, build UI
+ - makes easy to manage & build complex frontend
+
+=> When should I learn React?
+ - After mastering JavaScript
+ - most project don't need react in initial phase
+
+=> Why react was created?
+ - Ghost message problem
+ - State -> JS & UI -> DOM (Sync problem solution)
+ - Khan Academy, Unplash adopted React
+
+=> Don't learn React if:
+ - if you don't know how JS works or DOM works
+
+=> React learning process:
+ - go in-depth (Babel, Fibre, Virtual DOM, Diff. Algo, Hydration)
+ - by making projects (One topic at a time -> To-do, Calculator, Github API)
+
+=> React is library
+ - Framework vs Library
+
+=> Topics to learn
+ - core of React (State or UI manipulation, JSX)
+ - Component Reusability
+ - Reusing of components (props)
+ - How to propagate change (hooks)
+
+=> Additional Add on to React
+ - Router (React don't have Router)
+ - State management (React don't have state management)
+   -- Redux, Redux toolkit, zustand, context API
+ - class based component
+   -- legacy code
+ - BAAS Apps (Appwrite, Firebase, Superbase)
+   -- social media clone, e-commerce app 
+
+=> After React 
+ - React is not a complete solution in most case
+   -- no SEO, browser render of JS, no routing
+ - Framework
+   -- NextJS, Gatsby, Remix
+*/
+
+/*
+NOTE:
+=> React is Single Page Application (SPA)
+
+      For Website -> react-dom
+     🢅
+=> React
+     🢆
+      For Mobile Application -> react-native
+*/
